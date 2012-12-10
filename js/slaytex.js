01b015aa8e6d382cc88c1cd92baf5259a0bd0840
Slaytex <chris@chrisjacobs.com>
2012-12-09 16:34:19 -0800

    Changed Directory

---
+++ js/slaytex.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {							 
+	
+						
+$(".expand").click(function () {
+  $(".imgshowhide").slideToggle('slow');
+});
+
+
+$('.sonotube_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.sonotube_slides', 
+    pause:   1 
+});
+
+$('.soccer_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.soccer_slides', 
+    pause:   1 
+});
+
+$('.dharma_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.dharma_slides', 
+    pause:   1 
+});
+
+$('.spalding_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.spalding_slides', 
+    pause:   1 
+});
+
+$('.hotb_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.hotb_slides', 
+    pause:   1 
+});
+
+$('.imprint_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.imprint_slides', 
+    pause:   1 
+});
+
+$('.cctv_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.cctv_slides', 
+    pause:   1 
+});
+
+$('.blocks_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.blocks_slides', 
+    pause:   1 
+});
+
+$('.wordace_slides').cycle({ 
+    fx:     'fade', 
+    speed:   300, 
+    timeout: 3000000, 
+    next:   '.wordace_slides', 
+    pause:   1 
+});
+
+
+							 
+	
+});
+
+
